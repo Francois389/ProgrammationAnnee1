f6f7b64c36ebc046736352226470aa9a07c20d25
François SP <francois-sp@gmx.fr>
2023-04-13 10:29:46 +0200

    Tous fonctionne

--- workspaceQualiteDev/TestFx/src/application/Exercice1.java
+++ workspaceQualiteDev/TestFx/src/application/Exercice1.java
@@ -5,15 +5,25 @@
 package application; 
  
 import javafx.application.Application; 
-import javafx.stage.*; 
-import javafx.scene.*; 
+import javafx.event.ActionEvent; 
+import javafx.event.EventHandler; 
+import javafx.geometry.Pos; 
+import javafx.stage.Stage; 
+import javafx.scene.Scene; 
+import javafx.scene.control.Button; 
+import javafx.scene.control.TextField; 
+import javafx.scene.control.Label; 
+import javafx.scene.layout.BorderPane; 
+import javafx.scene.layout.HBox; 
+import javafx.scene.layout.VBox; 
+import javafx.scene.text.Text; 
 /**
  * Cette classe est la classe principale d'une application JavaFX.
  * On dit aussi le point d'entrée de l'application, car c'est la première classe qui 
  * sera exécutée au lancement de l'application (car elle hérite de la classe Application).
  * La fenêtre affichée aura pour titre "Première fenêtre JavaFX", une hauteur de 300 pixels
  * et une largeur de 500 pixels
- * @author C. Servières
+ * @author François de Saint Palais
  *
  */
 public class Exercice1 extends Application { 
@@ -25,9 +35,19 @@ public class Exercice1 extends Application {
         primaryStage.setHeight(300); // l'unité est le pixel
         primaryStage.setWidth(500); 
         
+        VBox racine = new VBox();
+        racine.setAlignment(Pos.CENTER);
+        
+        Text message = new Text("Test");
+        
         Label invitation = new Label("Heures");
         
+        racine.getChildren().add(message);
+        racine.getChildren().add(invitation);
        
+        
+        Scene scene = new Scene(racine);
+        primaryStage.setScene(scene);
         // on demande à ce que la fenêtre soit affichée
         primaryStage.show(); 
     } 
