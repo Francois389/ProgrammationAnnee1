a1b8a62eb3db904282a60a013b86489cdc9724fb
Francois SP <francois-sp@gmx.fr>
2023-05-12 11:32:50 +0200

    Création Projet Pile et File

---
+++ workspacePOO/PileEtFile/src/iut/info1/structure/Pile.java
@@ -0,0 +1,38 @@
+/**
+ * Pile.java                       12 mai 2023
+ * IUT de Rodez, pas de copyleft, pas de copyright
+ */
+package iut.info1.structure;
+
+/**
+ * //TODO Commenter la responsabilités de la classe Pile
+ * @author François de Saint Palais
+ *
+ */
+public class Pile {
+
+    public Pile() {
+        //TODO Écrire constructeur
+    }
+    
+    
+    public Pile empiler(Object element) {
+        //TODO ajouter element à this
+        return this;
+    }
+    
+    public boolean estVide() {
+        //TODO 
+        return false;
+    }
+    
+    public Pile depiler() {
+        //TODO supprimer l'élement au sommet de this
+        return this;
+    }
+    
+    public Object getSommet() {
+        //TODO Retourner l'élement au sommet de la pile
+        return null;
+    }
+}
